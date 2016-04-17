a34a81457125c3d37797a4fae8b9ad429882b163
yena <chocomaros@gmail.com>
2016-04-18 02:48:10 +0900

    SharedPreferences.java

---
+++ app/src/main/java/com/example/yena/losspreventionsystem/LPSSharedPreferences.java
@@ -0,0 +1,15 @@
+package com.example.yena.losspreventionsystem;
+
+/**
+ * Created by yena on 2016-04-18.
+ */
+public class LPSSharedPreferences {
+
+    public static final String NAME = "LPSSharedPreferences";
+    public static final String USER_ID = "UserID";
+    public static final String USER_PW = "UserPW";
+    public static final String USER_NAME = "UserName";
+    public static final String DISTANCE_SETTING = "DistanceSetting";
+    public static final String AUTO_LOGIN = "AutoLogin";
+
+}
