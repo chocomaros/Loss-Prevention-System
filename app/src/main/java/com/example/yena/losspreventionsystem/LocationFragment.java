09c00f89149beaeef9a65681504216140ac6ba83
yena <chocomaros@gmail.com>
2016-05-23 17:35:23 +0900

    원만들기 본격적으로 하기 전

--- app/src/main/java/com/example/yena/losspreventionsystem/LocationFragment.java
+++ app/src/main/java/com/example/yena/losspreventionsystem/LocationFragment.java
@@ -7,25 +7,61 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class LocationFragment extends Fragment {
 
-    private View view;
+    private Spinner spinnerItemSelection;
+    private ItemInfo itemInfo;
+    private ArrayList<ItemInfo> itemList;
+    private FrameLayout flLocation;
 
     public LocationFragment(){
-
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        itemList = new ArrayList<>();
+        itemList.addAll(LPSDAO.getItemInfo(getContext()));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_location,null);
+        View view = inflater.inflate(R.layout.fragment_location,null);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),R.layout.spinner_item);
+        for(int i=0; i<itemList.size(); i++){
+            adapter.add(itemList.get(i).name);
+        }
+        spinnerItemSelection = (Spinner)view.findViewById(R.id.spinner_location_item);
+        spinnerItemSelection.setAdapter(adapter);
+        spinnerItemSelection.setOnItemSelectedListener(itemSelectedListener);
         return view;
+    }
+
+    AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            itemInfo = itemList.get(position);
+            Toast.makeText(getContext(),itemInfo.name,Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 
     @Override
